5aa0be2e7f56e67706d9575e03450a5ae1f44fdd
leonardo <leonardo.alves@welvind.com>
2022-03-08 11:13:27 -0300

    fist commit

---
+++ index.php
@@ -0,0 +1,130 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link href="https://fonts.googleapis.com/css2? família=Play&display=swap" rel="stylesheet">
+    <title>Leonardo Developer</title>
+  </head>
+  <body>
+  
+  
+  
+<section class="navbar">
+    <a class="logo" href="">DevLeo</a>
+<ul>
+  <li><a class="active" href="">Home</a></li>
+  <li><a href="">About</a></li>
+  <li><a href="">Work</a></li>
+  <li><a href="">Contact</a></li>
+</ul>
+
+</section>
+
+<section class="banner">
+    <h1>Este é meu Website</h1>
+
+        <div class="banner2">
+            <h2>Eu sou o Leonardo, Developer Web JR.</h2>
+            <h3>Front-End</h3>
+        </div>
+</section>
+
+
+<section class="about">
+   
+            <div class="col-md-04">
+                <div class="box bg1">
+                    <img src="assets/img/myavatar.png" alt="">
+                </div>
+            </div>
+
+            <div class="col-md-06">
+                <div class="box bg2">
+                    <h2>Sobre</h2>
+                    <p>Olá, meu nome é Leonardo e sou desenvolvedor web jr.
+                        larguei meu trabalho e um cargo de liderança, com possibilidades altas
+                        de crescimento em uma rede de de varejo com mais de 40 lojas para seguir meu sonho, que é ser desenvolvedor web.
+                        buscando a especialização em Front-End para expressar minha criatividade,
+                        fiz este website para demonstrar um pouco das minhas habilidades.
+                </div>
+            </div>
+      
+</section>
+
+<section class="linguagem">
+    
+        <h1>Work</h1>
+
+<div class="container">
+        <div class="row icon">
+            <div class="col-md-3">
+                
+                <div class="icon1">
+                    <P>HTML5</P>
+                        <img src="assets/img/html5.png" alt="">
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="icon2">
+                    <P>CSS3</P>
+                        <img src="assets/img/css3.png" alt="">
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="icon1">
+                    <P>PHP</P>
+                        <img src="assets/img/php.png" alt="">
+                </div>          
+            </div>
+
+            <div class="col-md-3">
+                <div class="icon1">
+                    <P>JAVA-SCRIPT</P>
+                        <img src="assets/img/java-script.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</section>
+
+<footer id="rodape" class="foote1">
+    <div class="footer-content">
+      <h3 class="rodp">Leonardo Alves</h3>
+      <p class="rodp">Formas disponiveis para contato.</p>
+      <ul class="social-media">
+        <li class="link"><a href="https://api.whatsapp.com/qr/EYYCVATESJHDK1?autoload=1&app_absent=0"><img src="assets/img/whatsapp.png"></a></li>
+        <li class="link"><a href="https://www.instagram.com/leonard.alvs/"><img src="assets/img/instagram.png"></a></li>
+        <li class="link"><a href="https://mail.google.com/mail/u/1/#inbox?compose=CllgCJTGmSQrksVBzgvtfHJFBVClNHvrpdrVZKrGDlNZFPXtlKdlPKKWDncxLNZVVwpnbvrVbxV"><img src="assets/img/o-email.png"></a></li>
+        <li class="link"><a href="https://www.linkedin.com/in/leonardo-alves-194028185/"><img src="assets/img/linkedin.png"></a></li>
+      </ul>
+      <p class="Footer-bottom">Copyright &copy;2022. designed by Leonardo</p>
+    </div>
+  </footer>
+
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
